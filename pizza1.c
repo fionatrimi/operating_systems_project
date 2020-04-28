29dbafa8ba1cc22a61e2c283fd25c353cda90a39
Fiona Trimi <50248835+fionatrimi@users.noreply.github.com>
2020-04-28 23:28:21 +0300

    Update pizza1.c

--- pizza1.c
+++ pizza1.c
@@ -14,14 +14,28 @@ int t_prep = 1;//min for pizza preperation
 int t_bake = 10; //mins for pizza baking
 unsigned int seed;
 
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+//Variables
+int available_cooks;
+int available_ovens;
+double avg_time;
+double max_time;
+
+//Mutexes
+pthread_mutex_t mutex_available_cook = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_available_oven = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_avg_time = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_max_time = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_print = PTHREAD_MUTEX_INITIALIZER;
+
+//Conditions
+pthread_cond_t cond_no_available_cook = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond_no_available_oven = PTHREAD_COND_INITIALIZER;
 
 int main(int argc, char * argv[]){
 
   /*
-    * Checking if programm args are valid, 
-      prints the needed messages, 
+    * Checking if programm args are valid,
+      prints the needed messages,
       sets the values to n_cust and seed.
     * seed gets value 0 if invalid arguments is given (ex. string)
   */
@@ -58,62 +72,42 @@ int main(int argc, char * argv[]){
     pthread_join(threads[i], NULL);
   }
 
-  pthread_mutex_destroy(&lock);
+  pthread_mutex_destroy(&lock_cook);
   pthread_cond_destroy(&cond);
   return 0;
 
 }
+
 
 void * order(void *x){
   int id = *(int*)x;
 
   printf("Hello from order: %d\n", id);
   //start timer here
-  pthread_mutex_lock(&lock);
+  pthread_mutex_lock(&lock_cook);
   while (n_cook==0/*(n_cook<=1 && n_oven==0)||n_cook==0*/){
     printf("No cook/oven available. Order %d Blocked.\n", id);
-    pthread_cond_wait(&cond, &lock);
+    pthread_cond_wait(&cond, &lock_cook);
   }
+  //while(n_oven=0)
 
   printf("Order %d is executing\n", id);
   n_cook--;
   n_oven--;
-  pthread_mutex_unlock(&lock);
-  
+  pthread_mutex_unlock(&lock_cook);
+
   sleep(1);//sleep(t_prep);
   sleep(1);//sleep(t_bake);
 
-  pthread_mutex_lock(&lock);
+  pthread_mutex_lock(&lock_cook);
   printf("Successfull execution of order %d\n", id);
   n_cook++;
   n_oven++;
-  seed =id;
+  seed = id;
   pthread_cond_signal(&cond);
-  pthread_mutex_unlock(&lock);
+  pthread_mutex_unlock(&lock_cook);
   //end timer here
 
   pthread_exit(NULL);
 
-
-
-  /*int rc;
-  printf("Hello from order: %d\n", id);
-  rc = pthread_mutex_lock(&lock);
-
-  while (cooks==0){
-    printf("H paraggelia %d den brike paraskevasth. Blocked...\n", id);
-    rc= pthread_cond_wait(&cond, &lock);
-  }
-
-  printf("H paraggelia %d eksipiretitai.\n", id);
-  cooks--;
-  rc = pthread_mutex_unlock(&lock);
-  sleep(5);
-  rc=pthread_mutex_lock(&lock);
-  printf("H paraggelia %d eksipiretithike epitixos! \n", id);
-  cooks++;
-  rc = pthread_cond_signal(&cond);
-  rc = pthread_mutex_unlock(&lock);
-  */
-    
 }
