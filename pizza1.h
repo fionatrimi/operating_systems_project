902070f80fb4db703c83bc76701f6f0df4d14659
Fiona Trimi <50248835+fionatrimi@users.noreply.github.com>
2020-04-30 22:42:11 +0300

    Update pizza1.h

--- pizza1.h
+++ pizza1.h
@@ -3,14 +3,32 @@
 #include <pthread.h>
 #include <unistd.h>
 
+//Declaring functions
 void * order(void *x);
 void checking_mutex_action(int rc);
-const int n_cook = 6; //num of cooks
-const int n_oven = 5; //num of ovens
-const int t_orderlow = 1; //mins
-const int t_orderhigh = 5; //mins
-const int n_orderlow = 1; //pizzas
-const int n_orderhigh = 5; //pizzas
-const int t_prep = 1;//min for pizza preperation
-const int t_bake = 10; //mins for pizza baking
-unsigned int seed;
+
+//Delcaring variables
+extern const int n_cook; //num of cooks
+extern const int n_oven; //num of ovens
+extern const int t_orderlow; //mins
+extern const int t_orderhigh; //mins
+extern const int n_orderlow; //pizzas
+extern const int n_orderhigh; //pizzas
+extern const int t_prep; //min for pizza preperation
+extern const int t_bake; //mins for pizza baking
+extern unsigned int seed; //seed used for rand_r
+extern int available_cooks ;
+extern int available_ovens ;
+extern double avg_time;
+extern double max_time;
+
+//Declaring mutexes
+extern pthread_mutex_t mutex_available_cook;
+extern pthread_mutex_t mutex_available_oven;
+extern pthread_mutex_t mutex_avg_time;
+extern pthread_mutex_t mutex_max_time;
+extern pthread_mutex_t mutex_print;
+
+//Declaring conditions
+extern pthread_cond_t cond_available_cook;
+extern pthread_cond_t cond_available_oven;
